afe93ba1dbe66478071e75bcd15e18fc319654dc
Aditya Thakur <at894569@gmail.com>
2025-08-08 10:34:07 +0530

    changes

--- examples/script.js
+++ examples/script.js
@@ -1,372 +1,372 @@
-// //functions ke examples
+//functions ke examples
 
-// //function 
-
-
-// function square(a) {
-//   return a * a;
-// }
-
-// let b = square(3);
-// console.log(b);
+//function 
 
 
-// //function
+function square(a) {
+  return a * a;
+}
 
-// function check(val) {
-//   if (val % 2 === 0) {
-//     console.log("Even");
-//   } else {
-//     console.log("Odd");
-//   }
-// }
-
-// check(2);
+let b = square(3);
+console.log(b);
 
 
-// //function
+//function
 
-// function total(evt) {
-//   for (let i = 0; i < evt.length; i++) {
-//     console.log(evt[i]);
-//   }
-// }
+function check(val) {
+  if (val % 2 === 0) {
+    console.log("Even");
+  } else {
+    console.log("Odd");
+  }
+}
 
-// let arr = [10, 20, 30, 40, 50, 60];
-// total(arr);
-
-
-// //function
-
-// function num(val) {
-//   if (val === "a" || val === "e" || val === "i" || val === "o" || val === "u") {
-//     console.log("vowel");
-//   } else {
-//     console.log("not vowel");
-//   }
-// }
-
-// num("e");
+check(2);
 
 
+//function
 
-// //function
+function total(evt) {
+  for (let i = 0; i < evt.length; i++) {
+    console.log(evt[i]);
+  }
+}
 
-// function count(val) {
-//   let count = 0;
-//   for (let i = 0; i < val.length; i++) {
-//     if (val[i] % 2 === 0) {
-//       count++;
-//     }
-//   }
-//   return count;
-// }
+let arr = [10, 20, 30, 40, 50, 60];
+total(arr);
 
-// let number = [2, 3, 4, 5, 6, 7];
 
-// console.log(count(number));
+//function
+
+function num(val) {
+  if (val === "a" || val === "e" || val === "i" || val === "o" || val === "u") {
+    console.log("vowel");
+  } else {
+    console.log("not vowel");
+  }
+}
+
+num("e");
 
 
 
-// //callbacks
+//function
 
-// function sum(a, b) {
-//   console.log("sum", a + b);
-// }
+function count(val) {
+  let count = 0;
+  for (let i = 0; i < val.length; i++) {
+    if (val[i] % 2 === 0) {
+      count++;
+    }
+  }
+  return count;
+}
 
-// setTimeout(sum(3, 7), 3000);
+let number = [2, 3, 4, 5, 6, 7];
 
-// let fruits = ["apple", "mango", "banana"];
-
-// fruits.forEach(function (val) {
-//   console.log(val);
-// });
-
-
-// //callbacks
-
-// function age(a) {
-//   if (a >= 18) {
-//     console.log("you are adult");
-//   } else {
-//     console.log("you are not adult");
-//   }
-// }
-
-// function checkage(a, callback) {
-//   callback(a);
-// }
-// checkage(18, age);
-
-// //callbacks
-
-// setTimeout(function () {
-//   for (let i = 0; i <= 10; i++) {
-//     console.log(i);
-//   }
-// }, 2000);
-
-// //callbacks
-
-// function back(val) {
-//   val();
-// }
-
-// back(function () {
-//   console.log("hello");
-// });
+console.log(count(number));
 
 
-// //callbacks
 
-// function mul(a, b) {
-//   console.log(a * b);
-// }
+//callbacks
 
-// function op(x, y, mul) {
-//   mul(x, y);
-// }
+function sum(a, b) {
+  console.log("sum", a + b);
+}
 
-// op(3, 4, mul);
+setTimeout(sum(3, 7), 3000);
 
-// //closures....
+let fruits = ["apple", "mango", "banana"];
 
-// function val() {
-//   let a = 2;
-//   return function () {
-//     console.log(a);
-//   };
-// }
-// let evt = val();
-// evt();
+fruits.forEach(function (val) {
+  console.log(val);
+});
 
 
-// //closures....
+//callbacks
 
-// function counter() {
-//   let count = 0;
-//   return function () {
-//     count++;
-//     console.log(count);
-//   };
-// }
+function age(a) {
+  if (a >= 18) {
+    console.log("you are adult");
+  } else {
+    console.log("you are not adult");
+  }
+}
 
-// let v = counter();
-// v();
-// v();
-// v();
+function checkage(a, callback) {
+  callback(a);
+}
+checkage(18, age);
 
-// //closures
+//callbacks
 
-// function input(callback){
-//   setTimeout(() => {
-//     let name = "aditya";
-//     callback(name)
+setTimeout(function () {
+  for (let i = 0; i <= 10; i++) {
+    console.log(i);
+  }
+}, 2000);
 
-//   },2000)
-// }
-//  function say (name){
-//   console.log("hello " + name);
-//  }
-// input (say);
+//callbacks
 
-// //closures....
+function back(val) {
+  val();
+}
 
-// function cnt() {
-//   let ar = [10, 20, 30, 40, 50];
-
-//   return function () {
-//     ar.forEach(function (a) {
-//       console.log(a);
-//     });
-//   };
-// }
-
-// let p = cnt();
-// p();
+back(function () {
+  console.log("hello");
+});
 
 
-// //closures....
+//callbacks
+
+function mul(a, b) {
+  console.log(a * b);
+}
+
+function op(x, y, mul) {
+  mul(x, y);
+}
+
+op(3, 4, mul);
+
+//closures....
+
+function val() {
+  let a = 2;
+  return function () {
+    console.log(a);
+  };
+}
+let evt = val();
+evt();
 
 
-// function button (){
-//   let click = 0;
-//   let btn = document.getElementById("btn")
-//   btn.addEventListener("click", function (){
-//     click ++;
-//     console.log("clicked");
-//   })
+//closures....
+
+function counter() {
+  let count = 0;
+  return function () {
+    count++;
+    console.log(count);
+  };
+}
+
+let v = counter();
+v();
+v();
+v();
+
+//closures
+
+function input(callback){
+  setTimeout(() => {
+    let name = "aditya";
+    callback(name)
+
+  },2000)
+}
+ function say (name){
+  console.log("hello " + name);
+ }
+input (say);
+
+//closures....
+
+function cnt() {
+  let ar = [10, 20, 30, 40, 50];
+
+  return function () {
+    ar.forEach(function (a) {
+      console.log(a);
+    });
+  };
+}
+
+let p = cnt();
+p();
+
+
+//closures....
+
+
+function button (){
+  let click = 0;
+  let btn = document.getElementById("btn")
+  btn.addEventListener("click", function (){
+    click ++;
+    console.log("clicked");
+  })
   
-// }
+}
 
-// button();
-
-
+button();
 
 
-// //closures....
-
-// function some() {
-//   const username = "aditya";
-
-//   function call() {
-//     console.log(username);
-//   }
-
-//   setTimeout(call, 2000);
-// }
-
-// some();
 
 
-// //closures....
+//closures....
 
-// function start() {
-//   let sec = 0;
+function some() {
+  const username = "aditya";
 
-//   return function () {
-//     sec++;
-//     if (sec <= 5) {
-//       console.log(sec);
-//     } else {
-//       clearInterval(d);
-//     }
-//   };
-// }
+  function call() {
+    console.log(username);
+  }
 
-// let time = start();
-// let d = setInterval(time, 3000);
+  setTimeout(call, 2000);
+}
 
-// //promises....
+some();
 
 
-// let exam = new Promise (function (res, rej){
-//   let pass = true;
+//closures....
 
-//   if(pass){
-//     res("Pass")
-//   }else{
-//     rej("Fail")
-//   }
-// })
+function start() {
+  let sec = 0;
 
-// exam.then(function(data){
-//   console.log("result :", data);
-// })
+  return function () {
+    sec++;
+    if (sec <= 5) {
+      console.log(sec);
+    } else {
+      clearInterval(d);
+    }
+  };
+}
 
-// exam.catch(function(mm){
-//   console.log("result :", mm);
-// })
+let time = start();
+let d = setInterval(time, 3000);
+
+//promises....
 
 
-// //promises....
+let exam = new Promise (function (res, rej){
+  let pass = true;
+
+  if(pass){
+    res("Pass")
+  }else{
+    rej("Fail")
+  }
+})
+
+exam.then(function(data){
+  console.log("result :", data);
+})
+
+exam.catch(function(mm){
+  console.log("result :", mm);
+})
 
 
-// function abc (){
-//   return new Promise(function (res){
+//promises....
 
-//     setTimeout (function(){
-//       res("hello");
-//     }, 2000)
+
+function abc (){
+  return new Promise(function (res){
+
+    setTimeout (function(){
+      res("hello");
+    }, 2000)
     
-//   })
-// }
+  })
+}
 
 
-// abc().then(function(evt){
-//   console.log(evt);
-// });
+abc().then(function(evt){
+  console.log(evt);
+});
 
-// //promises....
+//promises....
 
-// function sub(val){
-//   return new Promise (function (res, rej){
-//     if (val.name1 && val.email){
-//       res("successfull")
-//     }else{
-//       rej("failed")
-//     }
-//   })
-// }
+function sub(val){
+  return new Promise (function (res, rej){
+    if (val.name1 && val.email){
+      res("successfull")
+    }else{
+      rej("failed")
+    }
+  })
+}
 
-// let data = {name1 : "aditya", email : "aditya@gmail.com"};
+let data = {name1 : "aditya", email : "aditya@gmail.com"};
 
-// sub(data).then(function(m){
-//   console.log(m);
-// })
-// sub(data).catch(function(vt){
-//   console.log(vt);
-// })
+sub(data).then(function(m){
+  console.log(m);
+})
+sub(data).catch(function(vt){
+  console.log(vt);
+})
 
-// //promises....
-
-
-// let email = new Promise (function (res, rej){
-//   let internet = true;
-//   if (internet){
-//     res("Email sent successfull")
-//   }else{
-//     rej("Failed connection lost")
-//   }
-// })
-
-// email.then(function(mail){
-//   console.log(mail);
-// })
-// email.catch(function(lost){
-//   console.log(lost);
-// })
+//promises....
 
 
-// //promises....
+let email = new Promise (function (res, rej){
+  let internet = true;
+  if (internet){
+    res("Email sent successfull")
+  }else{
+    rej("Failed connection lost")
+  }
+})
 
-// function result (marks){
-//   return new Promise (function (res, rej){
-//     if(marks >= 34){
-//       res("Pass")
+email.then(function(mail){
+  console.log(mail);
+})
+email.catch(function(lost){
+  console.log(lost);
+})
+
+
+//promises....
+
+function result (marks){
+  return new Promise (function (res, rej){
+    if(marks >= 34){
+      res("Pass")
     
-//     }else{
-//       rej("Fail")
+    }else{
+      rej("Fail")
       
-//     }
-//   })
-// }
+    }
+  })
+}
 
 
-// result(66)
-// .then(function (result){
-//   console.log(result);
-// })
-// .catch(function (result) {
-//   console.log(result);
+result(66)
+.then(function (result){
+  console.log(result);
+})
+.catch(function (result) {
+  console.log(result);
   
-// })
+})
 
 
-// //promise ....
+//promise ....
 
-// function login (){
-//   return new Promise (function (res, rej){
-//     function abc(){
-//       let data = prompt("enter your name");
-//       let pass = prompt("enter your password")
+function login (){
+  return new Promise (function (res, rej){
+    function abc(){
+      let data = prompt("enter your name");
+      let pass = prompt("enter your password")
 
-//       if(data === "aditya" && pass === "123"){
-//         res("login successful")
-//       }else{
-//         rej("login failed")
-//       }
-//     }
-//   })
-// }
+      if(data === "aditya" && pass === "123"){
+        res("login successful")
+      }else{
+        rej("login failed")
+      }
+    }
+  })
+}
 
-// login()
-// .then(function (msg){
-//   console.log(msg);
-// })
-// .catch(function(msg){
-//   console.log(msg);
-// })
+login()
+.then(function (msg){
+  console.log(msg);
+})
+.catch(function(msg){
+  console.log(msg);
+})
 
 
 //promise...
@@ -388,4 +388,5 @@ check ().then(function(vi){
 .catch(function (ho){
   console.log(ho);
 })
+
 
